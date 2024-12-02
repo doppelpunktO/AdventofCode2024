cc00089331810bf6b727394d7840ba5f3625628c
doppelpunktO <mail@annaneuwirth.de>
2024-12-02 15:41:44 +0100

    formatting changes

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -5,17 +5,20 @@ data = open("Day 1\locationIDs.txt", "r")
 list = data.read().split()
 list = [int(i) for i in list]
 data.close()
-# split lists
+
+# split & sort lists
 l1 = list[::2]
-l2 = list [1::2]
+l2 = list[1::2]
+
 # sort lists
 sortl1 = sorted(l1)
 sortl2 = sorted(l2)
-
 total = 0
+
 # dictionary & counter for P2
 sim = 0
 simdic = {}
+
 for i in range(0,len(sortl1)):
     if sortl1[i] in sortl2:
         # look how many appearences i has in l2
@@ -23,6 +26,7 @@ for i in range(0,len(sortl1)):
         simdic[sortl1[i]] = c
     # add every distance to total
     total += abs(sortl1[i] - sortl2[i])
+
 # multiply all similarities
 for i in simdic:
     sim += i * simdic[i]
