f8b3ad7fc92cec8bd3e7241af11c0f25f20e5dd8
doppelpunktO <mail@annaneuwirth.de>
2024-12-06 18:30:46 +0100

    finished day 3 part 2! :D

--- Day 3/Day 3.py
+++ Day 3/Day 3.py
@@ -3,24 +3,31 @@ import re
 
 # Part 1:
 # get input
-datap1 = open("input d3.txt", "r").read()
+data = open("input d3.txt", "r").read()
 
-# filter all mul operations
-mullist = re.findall(r"mul\(\d{1,3},\d{1,3}\)", datap1)
+# filter all important operations
+mullist = re.findall(r"mul\(\d{1,3},\d{1,3}\)", data)
+list = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", data)
 
+totalp1 = totalp2 = 0
 # strip all lines from "mul" and multiply X and Y
-totalp1 = 0
 for i in mullist:
      i = i.strip("mul()").split(",")
      totalp1 += int(i[0])*int(i[1])
 
+enable = True
+# do & donts enable / disable mul function
+for i in list:
+     if i == "do()" or i == "don't()":
+          enable = i == "do()"
+     else:
+          p = i.strip("mul()").split(",")
+          totalp2 += int(p[0]) * int(p[1]) * enable
+
 print("Part 1:", totalp1)
+print("Part 2:", totalp2)
 
-# Part 2
-# get input
-datap2 = open("input d3 p2.txt", "r").read()
 
-# Patterns for do's and don't's
-dopatt = re.compile(r"do\(\)")
-dontpatt = re.compile(r"don't\(\)")
-enable = 0
+
+
+
